985fdf9d482b79bc6edc46e26d7583b753e22d40
Crow-yoo <dbwltkd5984@gmail.com>
2025-01-06 15:06:28 +0900

    feat: 피드백(댓글 및 평가)기능 구현 및 설치

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { CrawlingModule } from './crawling/crawling.module';
 import { AiSummaryModule } from './ai-summary/ai-summary.module';
 import { NewsletterModule } from './newsletter/newsletter.module';
 import { AiSummaryService } from './ai-summary/ai-summary.service';
+import { FeedbackModule } from './feedback/feedback.module';
 
 @Module({
-  imports: [AuthModule, CrawlingModule, AiSummaryModule, NewsletterModule],
+  imports: [AuthModule, CrawlingModule, AiSummaryModule, NewsletterModule, FeedbackModule],
   controllers: [AppController],
   providers: [AppService, AiSummaryService],
 })
