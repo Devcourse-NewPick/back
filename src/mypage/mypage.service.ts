60e0a9b686c799db8c443aef614f952fcfa2f1b2
Crow-yoo <dbwltkd5984@gmail.com>
2025-01-16 19:08:01 +0900

    feat:mysql.schema에 추가 작업완료(테스트 완)

--- src/mypage/mypage.service.ts
+++ src/mypage/mypage.service.ts
@@ -6,7 +6,6 @@ export class MyPageService {
   constructor(private readonly prisma: MysqlPrismaService) {}
 
   async getProfile(userId: number) {
-    // DB에서 사용자 정보 조회
     return this.prisma.user.findUnique({
       where: { id: userId },
       select: {
@@ -14,16 +13,18 @@ export class MyPageService {
         email: true,
         username: true,
         profileImg: true,
+        interests: true,
+        timezone: true,
       },
     });
   }
 
   async getBookmarks(userId: number) {
-    // DB에서 좋아요(북마크)한 뉴스 목록 조회
     return this.prisma.feedback.findMany({
       where: { userId, likes: true },
       select: {
-        newsId: true,
+        newsId: true, // 개별 뉴스 ID
+        newsletterId: true, // 뉴스레터 ID
         comments: true,
         createdAt: true,
       },
