570016c224608502fc1d78c87bb2f86f15dfdae0
JaeYoung <kimjaeyoung650@gmail.com>
2025-01-24 22:40:20 +0900

    Feat: 테스트용 API 추가

--- src/crawling/crawling.controller.ts
+++ src/crawling/crawling.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { CrawlingRepository } from './crawling.repository';
 
 @Controller('crawling')
-export class CrawlingController {}
+export class CrawlingController {
+  constructor(
+    private readonly crawlingRepository: CrawlingRepository,
+  ) {}
+
+  @Get()
+  async getCrawledNews() {
+    return await this.crawlingRepository.getLatestCrawledNews();
+  }
+}
