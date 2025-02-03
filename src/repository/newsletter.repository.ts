0809ca4e1ce5d0763d35b97f71c42f902e422b8b
heechan <leeh09077@gmail.com>
2025-02-03 12:59:59 +0900

    fix: newsletter 리스트 가져오는데서 필요없는 필드 삭제

--- src/repository/newsletter.repository.ts
+++ src/repository/newsletter.repository.ts
@@ -10,6 +10,14 @@ export class NewsletterRepo {
     if (popular) {
       const orderBy = popular ? 'desc' : 'asc';
       return this.prisma.newsletter.findMany({
+        select: {
+          id: true,
+          title: true,
+          imageUrl: true,
+          categoryId: true,
+          viewcount: true,
+          createdAt: true,
+        },
         skip: offset,
         take: limit,
         orderBy: {
@@ -18,6 +26,14 @@ export class NewsletterRepo {
       });
     } else {
       return this.prisma.newsletter.findMany({
+        select: {
+          id: true,
+          title: true,
+          imageUrl: true,
+          categoryId: true,
+          viewcount: true,
+          createdAt: true,
+        },
         skip: offset,
         take: limit,
       });
